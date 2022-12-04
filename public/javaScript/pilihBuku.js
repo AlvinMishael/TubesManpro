3f1ad28dc5e3f3081504a87d950c600e21aacd36
WilliamNehem21 <6182001041@student.unpar.ac.id>
2022-12-04 22:34:56 +0700

    membenarkan halaman pilih buku

--- public/javaScript/pilihBuku.js
+++ public/javaScript/pilihBuku.js
@@ -1,5 +1,7 @@
-console.log(document.getElementById("pilih-buku").value);
+console.log(document.getElementById("pilih-buku"));
+//console.log(document.getElementById("pilih-buku").value);
 const dropDown = document.getElementById('pilih-buku'); 
+console.log(dropDown);
  dropDown.addEventListener('change', change);
 
 function change(e){
@@ -8,32 +10,32 @@ function change(e){
     if(document.getElementById("pilih-buku").value == 1 ){
         console.log(1);
         document.getElementById("gambarBuku").src="Book 1.jpeg";
-        document.getElementById("gambarBuku").className="mt-8 h-10"
+        document.getElementById("gambarBuku").className="-scale-75 mt-8 - ml-24 h-10"
         console.log(document.getElementById("gambarBuku").className)
 
     }
     else if (document.getElementById("pilih-buku").value == 2){
         console.log(2);
         document.getElementById("gambarBuku").src="Book 2.jpg";
-        document.getElementById("gambarBuku").className="mt-8 h-10"
+        document.getElementById("gambarBuku").className="object-fill mt-8 ml-24 h-40"
         console.log(document.getElementById("gambarBuku").className)
     }
     else if (document.getElementById("pilih-buku").value == 3){
         console.log(3);
         document.getElementById("gambarBuku").src="Book 3.jpg";
-        document.getElementById("gambarBuku").className="mt-8 h-10"
+        document.getElementById("gambarBuku").className="object-fill mt-8 ml-24 h-40"
         console.log(document.getElementById("gambarBuku").className)
     }
     else if (document.getElementById("pilih-buku").value == 4){
         console.log(4);
-        document.getElementById("gambarBuku").src="Book 4.webp";
-        document.getElementById("gambarBuku").className="mt-8 h-10"
+        document.getElementById("gambarBuku").src="Book 4.jpg";
+        document.getElementById("gambarBuku").className="object-fill mt-8 ml-24 h-40"
         console.log(document.getElementById("gambarBuku").className)
     }
     else if (document.getElementById("pilih-buku").value == 5){
         console.log(5);
         document.getElementById("gambarBuku").src="Book 5.jpeg";
-        document.getElementById("gambarBuku").className="mt-8 h-10"
+        document.getElementById("gambarBuku").className="object-fill mt-8 ml-24 h-40"
         console.log(document.getElementById("gambarBuku").className)
     }
     else{
