236b67a7b60621abf4c80f7fe99401394de438e4
WilliamNehemia6182001041 <6182001041@student.unpar.ac.id>
2022-12-16 21:16:08 +0700

    membuat bar chart interaktif

--- public/javaScript/pilihBuku.js
+++ public/javaScript/pilihBuku.js
@@ -4,15 +4,27 @@ const dropDown = document.getElementById('pilih-buku');
 console.log(dropDown);
  dropDown.addEventListener('change', change);
 
- new Chart(document.getElementById("grafik"), {
+ fetch('/barData1',{
+  method:'post',
+  headers:{"Content-Type":"application/json"}
+})
+.then(response => response.json())
+.then(data=>{
+  const dataLabel = [];
+  const dataFrekuensi = [];
+  for(let i = 0;i<data.length;i++){
+    dataLabel[i] = data[i].Source;
+    dataFrekuensi[i] = data[i].weight
+  }
+  new Chart(document.getElementById("grafik"), {
     type: 'horizontalBar',
     data: {
-      labels: ["Addam-Marbrand", "Aemon-Targaryen-(Maester-Aemon)", "Aerys-II-Targaryen", "Aggo", "Albett", "Alliser-Thorne", "Alyn", "Arya-Stark", "Balon-Swann", "Bran-Stark"],
+      labels: dataLabel,
       datasets: [
         {
           label: "Frekuensi",
           backgroundColor: ["#ff6700", "#ff7619","#ff8533","#ff954d","#ffa466", "#ffb380", "#ffc299", "#ffd1b3", "#ffe1cc", "#fff0e6"],
-          data: [100,90,80,70,60,50, 40, 30, 20, 15]
+          data: dataFrekuensi
         }
       ]
     },
@@ -24,6 +36,7 @@ console.log(dropDown);
       }
     }
   });
+})
 
 function change(e){
     console.log("method")
@@ -33,31 +46,195 @@ function change(e){
         document.getElementById("gambarBuku").src="Book 1.jpeg";
         document.getElementById("gambarBuku").className="-scale-75 mt-8 - ml-24 h-10"
         console.log(document.getElementById("gambarBuku").className)
-
+        fetch('/barData1',{
+          method:'post',
+          headers:{"Content-Type":"application/json"}
+        })
+        .then(response => response.json())
+        .then(data=>{
+          const dataLabel = [];
+          const dataFrekuensi = [];
+          for(let i = 0;i<data.length;i++){
+            dataLabel[i] = data[i].Source;
+            dataFrekuensi[i] = data[i].weight
+          }
+          new Chart(document.getElementById("grafik"), {
+            type: 'horizontalBar',
+            data: {
+              labels: dataLabel,
+              datasets: [
+                {
+                  label: "Frekuensi",
+                  backgroundColor: ["#ff6700", "#ff7619","#ff8533","#ff954d","#ffa466", "#ffb380", "#ffc299", "#ffd1b3", "#ffe1cc", "#fff0e6"],
+                  data: dataFrekuensi
+                }
+              ]
+            },
+            options: {
+              legend: { display: false },
+              title: {
+                display: false,
+                text: 'Predicted world population (millions) in 2050'
+              }
+            }
+          });
+        })
     }
     else if (document.getElementById("pilih-buku").value == 2){
         console.log(2);
         document.getElementById("gambarBuku").src="Book 2.jpg";
         document.getElementById("gambarBuku").className="object-fill mt-8 ml-24 h-40"
         console.log(document.getElementById("gambarBuku").className)
+        fetch('/barData2',{
+          method:'post',
+          headers:{"Content-Type":"application/json"}
+        })
+        .then(response => response.json())
+        .then(data=>{
+          const dataLabel = [];
+          const dataFrekuensi = [];
+          for(let i = 0;i<data.length;i++){
+            dataLabel[i] = data[i].Source;
+            dataFrekuensi[i] = data[i].weight
+          }
+          new Chart(document.getElementById("grafik"), {
+            type: 'horizontalBar',
+            data: {
+              labels: dataLabel,
+              datasets: [
+                {
+                  label: "Frekuensi",
+                  backgroundColor: ["#ff6700", "#ff7619","#ff8533","#ff954d","#ffa466", "#ffb380", "#ffc299", "#ffd1b3", "#ffe1cc", "#fff0e6"],
+                  data: dataFrekuensi
+                }
+              ]
+            },
+            options: {
+              legend: { display: false },
+              title: {
+                display: false,
+                text: 'Predicted world population (millions) in 2050'
+              }
+            }
+          });
+        })
     }
     else if (document.getElementById("pilih-buku").value == 3){
         console.log(3);
         document.getElementById("gambarBuku").src="Book 3.jpg";
         document.getElementById("gambarBuku").className="object-fill mt-8 ml-24 h-40"
         console.log(document.getElementById("gambarBuku").className)
+        fetch('/barData3',{
+          method:'post',
+          headers:{"Content-Type":"application/json"}
+        })
+        .then(response => response.json())
+        .then(data=>{
+          const dataLabel = [];
+          const dataFrekuensi = [];
+          for(let i = 0;i<data.length;i++){
+            dataLabel[i] = data[i].Source;
+            dataFrekuensi[i] = data[i].weight
+          }
+          new Chart(document.getElementById("grafik"), {
+            type: 'horizontalBar',
+            data: {
+              labels: dataLabel,
+              datasets: [
+                {
+                  label: "Frekuensi",
+                  backgroundColor: ["#ff6700", "#ff7619","#ff8533","#ff954d","#ffa466", "#ffb380", "#ffc299", "#ffd1b3", "#ffe1cc", "#fff0e6"],
+                  data: dataFrekuensi
+                }
+              ]
+            },
+            options: {
+              legend: { display: false },
+              title: {
+                display: false,
+                text: 'Predicted world population (millions) in 2050'
+              }
+            }
+          });
+        })
     }
     else if (document.getElementById("pilih-buku").value == 4){
         console.log(4);
         document.getElementById("gambarBuku").src="Book 4.jpg";
         document.getElementById("gambarBuku").className="object-fill mt-8 ml-24 h-40"
         console.log(document.getElementById("gambarBuku").className)
+        fetch('/barData4',{
+          method:'post',
+          headers:{"Content-Type":"application/json"}
+        })
+        .then(response => response.json())
+        .then(data=>{
+          const dataLabel = [];
+          const dataFrekuensi = [];
+          for(let i = 0;i<data.length;i++){
+            dataLabel[i] = data[i].Source;
+            dataFrekuensi[i] = data[i].weight
+          }
+          new Chart(document.getElementById("grafik"), {
+            type: 'horizontalBar',
+            data: {
+              labels: dataLabel,
+              datasets: [
+                {
+                  label: "Frekuensi",
+                  backgroundColor: ["#ff6700", "#ff7619","#ff8533","#ff954d","#ffa466", "#ffb380", "#ffc299", "#ffd1b3", "#ffe1cc", "#fff0e6"],
+                  data: dataFrekuensi
+                }
+              ]
+            },
+            options: {
+              legend: { display: false },
+              title: {
+                display: false,
+                text: 'Predicted world population (millions) in 2050'
+              }
+            }
+          });
+        })
     }
     else if (document.getElementById("pilih-buku").value == 5){
         console.log(5);
         document.getElementById("gambarBuku").src="Book 5.jpeg";
         document.getElementById("gambarBuku").className="object-fill mt-8 ml-24 h-40"
         console.log(document.getElementById("gambarBuku").className)
+        fetch('/barData5',{
+          method:'post',
+          headers:{"Content-Type":"application/json"}
+        })
+        .then(response => response.json())
+        .then(data=>{
+          const dataLabel = [];
+          const dataFrekuensi = [];
+          for(let i = 0;i<data.length;i++){
+            dataLabel[i] = data[i].Source;
+            dataFrekuensi[i] = data[i].weight
+          }
+          new Chart(document.getElementById("grafik"), {
+            type: 'horizontalBar',
+            data: {
+              labels: dataLabel,
+              datasets: [
+                {
+                  label: "Frekuensi",
+                  backgroundColor: ["#ff6700", "#ff7619","#ff8533","#ff954d","#ffa466", "#ffb380", "#ffc299", "#ffd1b3", "#ffe1cc", "#fff0e6"],
+                  data: dataFrekuensi
+                }
+              ]
+            },
+            options: {
+              legend: { display: false },
+              title: {
+                display: false,
+                text: 'Predicted world population (millions) in 2050'
+              }
+            }
+          });
+        })
     }
     else{
         document.getElementById("gambarBuku").src="allBook copy.png";
